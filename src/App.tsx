759d67f3dbee4bafe711712120f7bcca635b5aa1
vanya <vanyalush2000@gmail.com>
2025-03-20 20:17:41 +0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React, {FC} from 'react';
 import './App.css';
+import AuthForm from "./components/AuthForm";
 
 const App:FC = ()=> {
   return (
     <div className="App">
-      123
+      <AuthForm/>
     </div>
   );
 }
