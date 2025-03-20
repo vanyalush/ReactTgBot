818766268e72ad7cc723b3daf07be5a268281521
vanya <vanyalush2000@gmail.com>
2025-03-20 20:45:18 +0400

    first commit

---
+++ src/store/store.ts
@@ -0,0 +1,51 @@
+import {IUser} from "../models/IUser";
+import {makeAutoObservable} from "mobx";
+import AuthService from "../services/AuthService";
+
+export default class Store{
+    user= {} as IUser;
+    isAuth = false;
+    constructor() {
+        makeAutoObservable(this);
+    }
+    setAuth(bool: boolean) {
+        this.isAuth = bool;
+    }
+
+    setUser(user:IUser){
+        this.user = user;
+    }
+
+    async login(email: string, password: string){
+        try{
+            const response = await AuthService.login(email, password)
+            console.log(response)
+            localStorage.setItem("token", response.data.accessToken)
+            this.setAuth(true);
+            this.setUser(response.data.user);
+        } catch(e){
+            console.log(e);
+        }
+    }
+    async registration(email: string, password: string) {
+        try {
+            const response = await AuthService.registration(email, password);
+            console.log(response)
+            localStorage.setItem('token', response.data.accessToken);
+            this.setAuth(true);
+            this.setUser(response.data.user);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+    async logout(){
+        try{
+            const response = await AuthService.logout()
+            localStorage.removeItem("token")
+            this.isAuth = false;
+            this.setUser({} as IUser);
+        } catch(e){
+            console.log(e);
+        }
+    }
+}
